62bb33acf52ff813d2fc6dc80efb24225aef7a84
sabatsotadze121-lab <sabatsotadze121@gmail.com>
2025-10-14 19:49:30 +0400

    Add files via upload

---
+++ day 3/cw/cw3.py
@@ -0,0 +1,13 @@
+color = input("Enter your favorite color: ")
+print(color)
+
+number = int(input("Enter your favorite number: "))
+print(number)
+
+food = input("Enter your favorite food: ")
+print(food)
+
+drink = input("Enter your favorite drink: ")
+print(drink)
+
+
