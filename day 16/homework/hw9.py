1d0dcc0b33a24552154a3bccd8a6ce384aae7882
sabatsotadze121-lab <sabatsotadze121@gmail.com>
2025-11-21 23:14:20 +0400

    commit

--- day 16/homework/hw9.py
+++ day 16/homework/hw9.py
@@ -1,4 +1,40 @@
 # 9)შექმენით კალკულატორი როგორიც ჩვენ გავაკეთეთ,
 # დაუმატეთ სხვა მათემატიკური ოპერატორები,ასევე დაუმატეთ შედარების ოპერატორებიც
 
- 
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /, **, %, ==, !=, >, <, : ")
+
+if operator == "+":
+    print(number1 + number2)
+
+elif operator == "-":
+    print(number1 - number2)
+
+elif operator == "*":
+    print(number1 * number2)
+
+elif operator == "/":
+    print(number1 / number2)
+
+elif operator == "**":
+    print(number1 ** number2)
+
+elif operator == "%":
+    print(number1 % number2)
+
+elif operator == "==":
+    print(number1 == number2)
+
+elif operator == "!=":
+    print(number1 != number2)
+
+elif operator == ">":
+    print(number1 > number2)
+
+elif operator == "<":
+    print(number1 < number2)
+
+else:
+    print("Invalid operator!")
