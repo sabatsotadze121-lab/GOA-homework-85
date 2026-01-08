d2c0c36856faffa8fa5e2cbb24543669501bde63
sabatsotadze121-lab <sabatsotadze121@gmail.com>
2026-01-09 00:22:20 +0400

    commit

---
+++ day 28/classwork/cw2.py
@@ -0,0 +1,8 @@
+# 3) მოცემულია სია names = ["gIOrGi", "nINo", "lUkA"], დაბეჭდეთ თითოეული სახელი სწორად დაფორმატებული, სახელის პირველი ასო იყოს დიდი, დანარჩენი კი პატარა
+
+
+names = ["gIOrGi", "nINo", "lUkA"]
+
+print(names[0].capitalize())
+print(names[1].capitalize())
+print(names[2].capitalize())
