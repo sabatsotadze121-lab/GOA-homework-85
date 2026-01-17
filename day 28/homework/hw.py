d82ea2cd369f34bec4a4a30c5efef24783fce424
sabatsotadze121-lab <sabatsotadze121@gmail.com>
2026-01-17 15:06:15 +0400

    commit

--- day 28/homework/hw.py
+++ day 28/homework/hw.py
@@ -1,6 +1,2 @@
 # 1) მომხმარებელს შემოაყვანინე წინადადება. დაბეჭდე თითოეული სიტყვა ცალ–ცალკე for loop-ის გამოყენებით. თითოეული სიტყვა დაბეჭდე capitalize()-ით.
 
-sentence = input("Enter a sentence: ")
-words = sentence.split()
-for word in words:
-    print(word.capitalize())
