5744e9590fc8bda48f1e77d235ede9632bfcc25d
sabatsotadze121-lab <sabatsotadze121@gmail.com>
2026-01-22 21:28:53 +0400

    commit

--- day 30/homework/hw4.py
+++ day 30/homework/hw4.py
@@ -1,18 +1,15 @@
 # 4) შექმენით სტრინგის ცვლადი და ცარიელი სია. სტრინგში მყოფი დიდი ასოები გახადეთ პატარა და ამ სიაში ჩაამატეთ, ხოლო სტრინგში მყოფი პატარა ასოები გახადეთ დიდი და ასევე ჩააგდეთ ამ სიაში. დაპრინტეთ საბოლოო სია, გამოიყენეთ while ციკლი
 
 
-text = "PiTonI maGariA"
-new_list = []
+name = "Me vAr SaBa"
+result = []
+
 i = 0
-
-while i < len(text):
-    aa = text[i]
-    if aa.isupper():
-        new_list.append(aa.lower())
-    elif aa.islower():
-        new_list.append(aa.upper())
+while i < len(name):
+    if name[i] == name[i].upper():
+        result.append(name[i].lower())
     else:
-        new_list.append(aa) 
-    i = i + 1
+        result.append(name[i].upper())
+    i - i + 1
 
-print(new_list)
+print(result)
--- day 30/homework/hw6.py
+++ day 30/homework/hw6.py
@@ -1,13 +1,13 @@
 # 6) შექმენით სტრინგებით სავსე სია, წაშალეთ ის სტრინგ მონაცემთა ტიპის ელემენტები რომლებიც არიან 5-ზე მეტი სიგრძეში ან დგანან კენტ ინდექსზე. გამოიყენეთ remove() ფუნქცია.
 
-strings = ["apple", "banana", "cat", "elephant", "dog", "python"]
+words = ["apple", "banana", "cat", "elephant", "dog", "python"]
 i = 0
 
-while i < len(strings):
-    if len(strings[i]) > 5 or i % 2 != 0:
-        strings.remove(strings[i])
+while i < len(words):
+    if len(words[i]) > 5 or i % 2 != 0:
+        words.remove(words[i])
         
     else:
         i = 1 + 1
 
-print(strings)
+print(words)
